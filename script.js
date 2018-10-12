8e83baf35661e183691c6aca132d0b5cbed637a2
28639 <28639@student.pwsztar.edu.pl>
2018-10-12 15:59:02 +0200

    lab3

---
+++ script.js
@@ -0,0 +1,68 @@
+let questions =
+    [
+        {
+            number: 1,
+            question:'1. Jakiego typu grą jest Call of Duty?',
+            answers: ['FPS','MMOPRG','Zręcznościowa','Logiczna'],
+            correct_answer:'FPS'
+        },
+        {
+            number: 2,
+            question:'2. Jakiego koloru jest niebieski maluch?',
+            answers: ['Niebieskiego', 'Czerwonego','Żółtego','Czarnego'],
+            correct_answer:'Niebieskiego'
+        },
+        {
+            number: 3,
+            question:'3. Jaka jest prędkość światła?',
+            answers:['Nie wiem', '123 456 7 kmh','300 000 kmsek','90kmh'],
+            correct_answer:'300 000 kmsek'
+        },
+
+        {
+            number:4,
+            question:'4. Ziemia jest?',
+            answers:['Kulą','Płaska','Elipsoidą','Prostokątem'],
+            correct_answer:'Elipsoidą'
+        },
+        {
+            number: 5,
+            question:'5. Modelem mercedessa jest?',
+            answers:['W123','E12','A5','WD40'],
+            correct_answer:'W123'
+        },
+        {
+            number:6,
+            question:'6. Top Gun to film...?',
+            answers:['Sensacyjny','Dramat','Komedia','Wojenny'],
+            correct_answer:'Sensacyjny'
+        },
+        {
+            number:7,
+            question:'7. Stolica Australii jest...?',
+            answers:['Melbourne','Canberra','Sydney','Delhi'],
+            correct_answer:'Canberra'
+        },
+        {
+            number:8,
+            question:'8. Więcej niż jedno zwierze to?',
+            answers:['Lama','Okoń','Linijka','Wataha'],
+            correct_answer:'Wataha'
+        },
+        {
+            number:9,
+            question:'9. Co pływa po kanale weneckim?',
+            answers:['Gondola','Ponton','Galera','Żaglówka'],
+            correct_answer:'Gondola'
+        },
+        {
+            number:10,
+            question:'10. Jak nazywa się tradycyjny napój Anglików pity po południu?',
+            answers:['Kawa','Mleko','Herbata','Sok'],
+            correct_answer:'Herbata'
+        },
+    ]
+
+
+let button=document.querySelector('.next');
+button.addEventListener('click',function (evt) {  })
